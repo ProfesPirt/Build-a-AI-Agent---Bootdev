4a0942e40796b30097c8eebe2cb0e99722078d0d
ProfesPirt <gillisjames4@gmail.com>
2026-07-01 23:43:36 -0400

    Added command line argument handling from argparse module

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
@@ -19,10 +24,13 @@ def main():
         messages=[
             {
                 "role": "user",
-                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+                "content": args.user_prompt,
             }
         ]
     )
+    if not chat_completed.usage:
+        raise RuntimeError("It seems the API request has failed try again?")
+    print(f"Prompt tokens: {chat_completed.usage.prompt_tokens}\nResponse tokens: {chat_completed.usage.completion_tokens}")
     print(chat_completed.choices[0].message.content)
 
 
